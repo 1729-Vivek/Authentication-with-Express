04b2b55a42ccb23137a7ed01823ed54004f85274
Vivek <itspersonal4vivek@gmail.com>
2024-03-17 15:00:32 +0530

    app file

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+const authRouter=require('./router/authRoute');
+
+app.use(express.json());
+
+app.use('/api/auth/',authRouter)
+
+
+
+app.use('/',(req,res)=>{
+    res.status(200).json({data : 'JWTauth Server'})
+});
+
+module.exports=app;
