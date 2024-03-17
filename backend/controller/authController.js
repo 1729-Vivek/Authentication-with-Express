602c7a89098b7af3107d0d1d5b3eb82ba5fc76bb
Vivek <itspersonal4vivek@gmail.com>
2024-03-17 19:16:37 +0530

    added data verification for userschema and user model

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,10 +1,37 @@
-const signup=(req,res,next)=>{
+const signup=async(req,res,next)=>{
     const {name,email,password,confirmPassword}=req.body;
     console.log(name,email,password,confirmPassword);
-    return res.status(200).json({
-        success:true,
-        data:{}
-    })
+
+
+    try{
+        const userInfo=UserModel(req.body);  //we wrote this because we defined userModel in our userSchema.js file where we require name ,email,password,confirmpassword
+        //if we have not defined(or structure is not same) then we have to write it separately
+    
+        const result=await userInfo.save();
+    
+        return res.status(200).json({
+            success:true,
+            data:result
+        });
+    }
+    catch(e)
+    {
+        if(e.code===11000)
+        {
+            return res.status(400).json({
+                success:false,
+                message:'Account already exists with provided email id'
+            })
+        }
+        return res.status(400).json({
+            success:false,
+            message:e.message
+        })
+    }
+
+
+
+    
 }
 module.exports={
     signup
