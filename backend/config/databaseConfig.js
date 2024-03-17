b63eb70bdbdf6836a916ae8327856e2880358c3c
Vivek <itspersonal4vivek@gmail.com>
2024-03-17 18:19:35 +0530

    added process environment to get access to third party app

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const MONGODB_URL= process.env.MONGODB_URL||"mongodb://localhost:27017/users"
 
@@ -6,7 +7,7 @@ const databaseconnect=()=>{
     mongoose
     .connect(MONGODB_URL)
     .then((conn)=>console.log(`Connected to DB:${conn.connection.host}`))
-    .catch((err)=>console.log(err.messae));
+    .catch((err)=>console.log(err.message));
 }
 
-module.exports=databaseconnect;
+module.exports=databaseconnect
