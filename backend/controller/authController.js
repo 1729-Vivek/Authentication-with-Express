55d6d3e9831c5b0475bfd288b25c97c4e32eec9d
Vivek <itspersonal4vivek@gmail.com>
2024-03-20 02:44:42 +0530

    Error resolved:changed userModel to UserModel

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -56,10 +56,54 @@ const signup=async(req,res,next)=>{
         })
     }
 
+}
 
+const signin=async (req,res)=>{
+    const {email,password}=req.body;
 
+    if(!email||!password)
+    {
+        return res.status(400).json({
+            success:false,
+            message:"Every field is mandatory"
+    })
+    }
+    try{
+        const user=await UserModel
+        .findOne({
+            email
+        })
+        .select('+password');
     
+        if(!user||user.password!==password)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"invalid credentials"
+            })
+        }
+        const token=user.jwtToken();
+        user.password=undefined;
+    
+        const cookieOption={
+            maxAge:24*60*60*1000,
+            httpOnly:true
+        }
+        res.cookie("token",token,cookieOption);
+        res.status(200).json({
+            success:true,
+            data:user
+        })
+    }catch(err){
+        
+            return res.status(400).json({
+                success:false,
+                message:err.message
+            })
+    
+    }
+   
 }
 module.exports={
-    signup
+    signup,signin
 }
