37844acd5c1f5a75c121dac411c7ec3a23538fb1
Vivek <itspersonal4vivek@gmail.com>
2024-03-26 01:08:03 +0530

    In last commit ,I had simply added CORS but it was not working properly because of CORS wrong destination and origin. In app.js , I changed origin from https://localhost:3000 to https://192.168.144.47:3000 . Also ,I allowed access from Origin ,Methods and Headers

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,18 @@ databaseconnect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:[process.env.CLIENT_URL],
-    credentials:true
-}))
+    origin: 'http://192.168.144.47:3000',
+    credentials: true
+}));
+
+app.use('/api', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.144.47:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+
 
 app.use('/api/auth/',authRouter)
 
